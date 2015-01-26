0a55235ff601ae90cd7532be6e59b9131fac5cdd
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-01-27 01:13:52 +0200

    fix-up

--- src/scheduler/__init__.py
+++ src/scheduler/__init__.py
@@ -5,10 +5,10 @@ This is a main package of a web-app
 
 from os.path import dirname, realpath
 
-__author__ = 'Sviatoslav Sydorenko'
-__email__ = 'svyatoslav@sydorenko.org.ua'
-__date__ = '2014-12-19'
-__appname__ = 'Student Scheduler'
+__author__ = 'Oleksandr Sochka'
+__email__ = 'sasha.sochka@gmail.com'
+__date__ = '2015-24-02'
+__appname__ = 'KPI Export'
 __version__ = '0.1'
 __config__ = 'app.yaml'
 
